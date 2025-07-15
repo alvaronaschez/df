eb2a59e6af55687f6e8fbbb6a76b22067e95c582
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-07-15 19:05:33 +0200

    stuff

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+mkdir -p $HOME/.config
+mkdir -p $HOME/.local/bin
+mkdir -p $HOME/.local/share
+
 echo 'source ~/.config/bash/bashrc.bash' >> ~/.bashrc
 echo '$include ~/df/config/readline/inputrc' >> ~/.inputrc
 echo 'PATH="$PATH:/usr/sbin"' >> ~/.profile
+
+cp $HOME/df/home/dot-local/share/battery.service \
+    /etc/systemd/system/
+systemd enable battery.service
 
 ./sync.sh
 
