ba3369a50b9f3667f1ce8122afb9f410d53cb52c
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-07-15 19:31:54 +0200

    stuff

--- home/dot-local/bin/battery_service.sh
+++ home/dot-local/bin/battery_service.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
-echo 60 > /sys/class/power_supply/BAT0/charge_control_start_threshold
-echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold
+
+if [ -d /sys/class/power_supply/BAT0 ]
+then
+    echo 60 > /sys/class/power_supply/BAT0/charge_control_start_threshold
+    echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold
+fi
+
+if [ -d /sys/class/power_supply/BAT1 ]
+then
+    echo 60 > /sys/class/power_supply/BAT1/charge_control_start_threshold
+    echo 80 > /sys/class/power_supply/BAT1/charge_control_end_threshold
+fi
 
