6e28af1a6e8b56c3fb02312b30c2ced0fae0cb2f
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-07-15 19:12:28 +0200

    stuff

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+# create some dirs so the symbolic links work as expected
 mkdir -p $HOME/.config
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/share
 
+# create a couple of links by hand
 echo 'source ~/.config/bash/bashrc.bash' >> ~/.bashrc
 echo '$include ~/df/config/readline/inputrc' >> ~/.inputrc
 echo 'PATH="$PATH:/usr/sbin"' >> ~/.profile
 
-cp $HOME/df/home/dot-local/share/battery.service \
+# enable battery service
+sudo cp $HOME/df/home/dot-local/share/battery.service \
     /etc/systemd/system/
-systemd enable battery.service
+sudo systemctl enable battery.service
 
+# use stow
 ./sync.sh
 
 
