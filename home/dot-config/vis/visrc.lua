90786898a709ef4d3588b4f56471d3412fdb551a
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-07-24 20:12:40 +0200

    visrc

--- home/dot-config/vis/visrc.lua
+++ home/dot-config/vis/visrc.lua
@@ -4,10 +4,14 @@ require('plugins/vis-lspc')
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
-	vis:command('set theme solarized')
+	-- https://github-wiki-see.page/m/martanne/vis/wiki/Themes
+	--vis:command('set theme base16-dracula')
+	--vis:command('set theme base16-circus')
+	--vis:command('set theme base16-eighties')
+	vis:command('set theme seti')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
 	-- Your per window configuration options e.g.
-	-- vis:command('set number')
+	vis:command('set relativenumbers')
 end)
