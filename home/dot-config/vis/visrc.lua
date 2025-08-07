43a02af3fae2b65a620a2aca9536d3cf16d38871
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-08-07 10:30:51 +0200

    vis config

--- home/dot-config/vis/visrc.lua
+++ home/dot-config/vis/visrc.lua
@@ -1,15 +1,25 @@
--- load standard vis module, providing parts of the Lua API
+-- loa vis module, providing parts of the Lua API
 require('vis')
 require('plugins/vis-lspc')
 
+
 vis.events.subscribe(vis.events.INIT, function()
+	-- colors not working well in foot if $TERM equals foot
+	--os.execute('if [ $TERM == foot ]; then export TERM=tmux-256color; fi')
+
 	-- Your global configuration options
 	-- https://github-wiki-see.page/m/martanne/vis/wiki/Themes
-	vis:command('set theme base16-circus')
-	--vis:command('set theme base16-eighties')
+	--vis:command('set shell /usr/bin/bash')
+	--vis:command('set theme base16-circus')
+	--vis:command('set theme seti')
+	vis:command('set theme minimal-light')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
 	-- Your per window configuration options e.g.
 	vis:command('set relativenumbers')
+	vis:command('set autoindent on')
+	vis:command('set colorcolumn 80')
+	vis:command('set cursorline on')
+	vis:command('set ignorecase on')
 end)
